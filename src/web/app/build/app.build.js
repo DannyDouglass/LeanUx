0a3a7a0ea0e71de1ceb36728d0ee79c48085bf38
Danny Douglass <ddouglass@gmail.com>
2013-03-01 16:27:46 -0500

    adding build optimization configuration file and temporary notes; todo: move temporary web notes to markdown file

---
+++ src/web/app/build/app.build.js
@@ -0,0 +1,10 @@
+({
+	appDir: "../",
+	dir: "../../dist",
+	mainConfigFile: "../main.js",
+	name: "main",
+	optimizeCss: "standard"
+})
+
+/* use if appDir is not same as baseUrl */
+/* 	baseUrl: "" */
