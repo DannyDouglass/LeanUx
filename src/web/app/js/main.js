48aca287d98ac6018b10511efa4c8c298084054e
Danny Douglass <ddouglass@gmail.com>
2013-03-04 12:32:48 -0500

    added twitter bootstrap module and dependencies; setup navigation header per new hire mock screen design

--- src/web/app/js/main.js
+++ src/web/app/js/main.js
@@ -1,8 +1,15 @@
 require.config({
   paths: {
-    "jquery" : "../lib/jquery/jquery",
-    "underscore" : "../lib/underscore-amd/underscore",
-    "backbone" : "../lib/backbone-amd/backbone"
+    "jquery" : "../lib/jquery/jquery.min",
+    "bootstrap" : "../lib-local/bootstrap/js/bootstrap.min",
+    "underscore" : "../lib/underscore-amd/underscore-min",
+    "backbone" : "../lib/backbone-amd/backbone-min"
+  },
+  shim: { 
+  	"bootstrap" : {
+  		deps: ["jquery"],
+  		exports: "$.fn.dropdown"
+  	}
   }
 });
 
