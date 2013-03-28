3655f84ed39a0038cf3bb56703286715ff4bb4f7
armw4 <antwan.wimberly@gmail.com>
2013-03-28 19:26:02 -0400

    Remove previous button if on first. Change text of next button to Complete New Hire Entry if on last step. Trigger events when next, previoius, or done is clicked.

--- public/app/js/views/newhirenextpreviousview.js
+++ public/app/js/views/newhirenextpreviousview.js
@@ -6,9 +6,38 @@ define(
         tagName: "div",
         className: "actions",
         template: wizardNextPreviousTemplate,
+        firstStep: 1,
+        lastStep: 5,
 
         initialize: function() {
+            this.step = this.options.step;
+        },
 
+        triggers: {
+            "click #btn-previous .btn": "step:previous"
+        },
+
+        events: {
+            "click #btn-next-done .btn": "nextOrDone"
+        },
+
+        ui: {
+            previousButton: '#btn-previous .btn',
+            nextOrDoneButton: '#btn-next-done .btn'
+        },
+
+        onRender: function(){
+            if (this.step == this.firstStep)
+                this.ui.previousButton.hide();
+            else if (this.step == this.lastStep)
+                this.ui.nextOrDoneButton.text('Complete New Hire Entry');
+        },
+
+        nextOrDone: function(){
+            if (this.step == lastStep)
+                this.trigger("step:done");
+            else
+                this.trigger("step:next");
         }
     });
 
